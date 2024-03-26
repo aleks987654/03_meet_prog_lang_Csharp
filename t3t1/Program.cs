66df81315e63257af2ff851ce6b8a075b6937166
Aleksandr Trushkov <aleksmount@yandex.ru>
2024-03-26 22:35:07 +0700

    add t3t1 - своя прога

---
+++ t3t1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100. 
+// Найдите количество элементов массива, 
+// значения которых лежат в отрезке [10, 90].
+
+int[] numbers = new int[10];
+
+for (int i = 0; i < 10; i++)
+{
+    numbers[i] = new Random().Next(1, 101);
+    Console.Write($"{numbers[i]} ");
+}
+
+int minRange = 10;
+int maxRange = 90;
+int amount = 0;
+
+foreach (int e in numbers)
+{
+    if ((e >= minRange) && (e <= maxRange))
+    {
+        amount++;
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine(amount);
