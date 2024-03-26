a42cefdef0fb26fa3251246f146b854c4d8be3ca
Aleksandr Trushkov <aleksmount@yandex.ru>
2024-03-26 23:05:12 +0700

    add t3t3s

---
+++ t3t3s/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+
+//Тело класса будет написано студентом. Класс обязан иметь статический метод PrintResult()
+class UserInputToCompileForTest
+{ 
+    // Разница между максимальным и минимальным элементами массива
+    
+    // Нахождение минимума массива
+    public static double FindMin(double[] numbers)
+    {
+        //Напишите свое решение здесь
+        double min = numbers[0];
+        foreach (double e in numbers)
+        {
+            if (e < min)
+            {
+                min = e;
+            }
+        }
+        return min;
+    }
+    
+    // Нахождение максимума массива
+    public static double FindMax(double[] numbers)
+    {
+        //Напишите свое решение здесь
+        double max = numbers[0];
+        foreach (double e in numbers)
+        {
+            if (e > max)
+            {
+                max = e;
+            }
+        }
+        return max;
+    }
+    
+    
+    
+    public static void PrintResult(double[] array)
+    {
+        //Напишите свое решение здесь
+        Console.WriteLine(FindMax(array) - FindMin(array));
+    }
+}
+
+//Не удаляйте и не меняйте класс Answer!
+class Answer
+{
+    public static void Main(string[] args)
+    {
+        double[] array;
+        
+
+        if (args.Length >= 1) {
+            // Объединяем все аргументы командной строки в одну строку
+            string joinedArgs = string.Join(" ", args);
+
+            // Разделяем строку по запятой с пробелом и преобразуем в массив целых чисел
+            array = joinedArgs.Split(", ")
+                                  .Select(double.Parse)
+                                  .ToArray();
+            
+            // Теперь arr содержит преобразованные в целые числа из командной строки
+        
+        } else {
+           // Если аргументов на входе нет
+            array = new double[] {0.25, 5.4, 1.3, 2.1, 3.8, 5.2, 3.01}; // Создание массива
+        }
+        UserInputToCompileForTest.PrintResult(array);
+    }
+}
