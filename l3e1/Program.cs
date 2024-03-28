b68b5e5a3266bc6c97847fb1dd88f8742bbf043e
Aleksandr Trushkov <aleksmount@yandex.ru>
2024-03-28 21:06:18 +0700

    add l3e1 - функция формула

---
+++ l3e1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1: Вычислить значение формулы
+//          a x b
+//          -----   ,
+//          c + d
+// где a, b, c, d - некоторые целые числа.
+// Решение оформить в виде функции.
+
+
+// функция
+double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denomenator = c + d;
+    double result = numenator / denomenator;
+    return result;
+}
+
+Console.WriteLine(CalculateFormula(1, 2, 3, 4));
+
+ 
