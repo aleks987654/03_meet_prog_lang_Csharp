96f61dbd10005af70ecabafcd861a53db798c505
Aleksandr Trushkov <aleksmount@yandex.ru>
2024-03-28 22:58:58 +0700

    add l3e3 - ssilochniy tip

---
+++ l3e3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Функция "Обнуление чётных значений элементов массива"
+
+void ZeroEvenElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (var item in arr)
+    {
+        Console.Write($"{item} ");
+    }
+}
+
+int[] array = { 1, 2, 3, 4, 5 };
+ZeroEvenElements(array);
+PrintArray(array);
