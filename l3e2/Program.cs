2af92c0f68cb78c5e8b454ca4f6eb50f8fa77639
Aleksandr Trushkov <aleksmount@yandex.ru>
2024-03-28 22:21:33 +0700

    add l3e2 - функция PrintSquares

---
+++ l3e2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2: Вывод на экран квадратов чисел от 1 до N
+
+// функция
+void PrintSquares(int limit)
+{
+    int i = 0;
+    while (i <= limit)
+    {
+        Console.Write($"{i * i} ");
+        i++;
+    }
+    Console.WriteLine();
+}
+
+PrintSquares(5);
+PrintSquares(10);
+PrintSquares(15);
