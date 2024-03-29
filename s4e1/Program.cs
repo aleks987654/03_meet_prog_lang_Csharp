7577adeaf49c969a5ef5bb5ff5b7aaae60155728
Aleksandr Trushkov <aleksmount@yandex.ru>
2024-03-29 21:20:33 +0700

    s4e1 - исправил косяки

--- s4e1/Program.cs
+++ s4e1/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Задание 1
 // Задайте одномерный массив, заполненный случайными числами.
 // Определите количество простых чисел в этом массиве.
+// !!! "чисел" а не элементов
 
 // Простое число - натуральное число, отличное от 1 
 // и делится без остатка только на 1 и на себя.
@@ -10,7 +11,7 @@ void FillArray(int[] collection)
 {
     for (int i = 0; i < collection.Length; i++)
     {
-        collection[i] = new Random().Next(1, 1000);        
+        collection[i] = new Random().Next(1, 10);
     }
 }
 
@@ -28,14 +29,14 @@ void PrintArray(int[] collection)
 // (через массив простых чисел от 2 до 97)
 int AmountPrimeNumbersArray(int[] collection)
 {
-    int[] prime_numbers = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97};
+    int[] prime_numbers = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
     int result = 0;
     Console.WriteLine();
     foreach (var item in collection)
     {
         foreach (var item_prime in prime_numbers)
         {
-            if (item == item_prime) 
+            if (item == item_prime)
             {
                 result++;
                 Console.Write($"{item} ");
@@ -52,6 +53,17 @@ int AmountPrimeNumbersRemDiv(int[] collection)
     int result = 0;
     bool is_prime_number = true;
     Console.WriteLine();
+
+    for (int i = 0; i < collection.Length; i++)
+    {
+        for (int j = i + 1; j < collection.Length; j++)
+        {
+            if (collection[j] == collection[i]) collection[j] = 0;
+        }
+    }
+
+    PrintArray(collection);
+    Console.WriteLine();
     foreach (var item in collection)
     {
         is_prime_number = true;
@@ -59,7 +71,7 @@ int AmountPrimeNumbersRemDiv(int[] collection)
         {
             if (item % i == 0) is_prime_number = false;
         }
-        if (is_prime_number && (item > 1)) 
+        if (is_prime_number && (item > 1))
         {
             Console.Write($"{item} ");
             result++;
@@ -81,3 +93,15 @@ PrintArray(arr);
 int amount2 = AmountPrimeNumbersRemDiv(arr);
 Console.WriteLine();
 Console.WriteLine($"{amount2} простых чисел в массиве");
+
+int[] arr_example1 = { 1, 3, 4, 19, 3 };
+PrintArray(arr_example1);
+int amount3 = AmountPrimeNumbersRemDiv(arr_example1);
+Console.WriteLine();
+Console.WriteLine($"{amount3} простых чисел в массиве");
+
+int[] arr_example2 = { 4, 3, 4, 1, 9, 5, 21, 13 };
+PrintArray(arr_example2);
+int amount4 = AmountPrimeNumbersRemDiv(arr_example2);
+Console.WriteLine();
+Console.WriteLine($"{amount4} простых чисел в массиве");
