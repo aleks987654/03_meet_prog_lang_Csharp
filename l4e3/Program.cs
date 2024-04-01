83cad6d9c76a7b3f7ca10b5a88afae040156e4c7
Aleksandr Trushkov <aleksmount@yandex.ru>
2024-04-01 22:42:44 +0700

    add l4e3 - string

---
+++ l4e3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Лекция 4. Двумерные массивы
+// Задача 3
+// Считать с консоли строку, состоящую из цифр и латинских букв.
+// Сформировать новую строку, состоящую из букв исходной строки.
+
+// Функция извлечения букв из строки
+string GetLettersFromStr(string s)
+{
+    string letters = "";
+    foreach (var e in s)
+    {
+        if (char.IsAsciiLetter(e))
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+
+// **********   Тело программы   **********
+string? str = Console.ReadLine();
+string? letters = GetLettersFromStr(str);
+Console.WriteLine(letters);
