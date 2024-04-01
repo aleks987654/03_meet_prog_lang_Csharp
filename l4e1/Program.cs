0c52186f0cdb37c4d0ad34e41db5e6b23acfa614
Aleksandr Trushkov <aleksmount@yandex.ru>
2024-04-01 21:43:34 +0700

    add l4e1 - двумерные массивы

---
+++ l4e1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Лекция 4. Двумерные массивы
+// Задача 1
+// Создать двумерный массив с размерами 3 х 5, состоящий из целых чисел
+// Вывести его элементы на экран
+
+// Функция создания двумерного массива целых чисел 
+// и заполнения его случайными числами
+int[,] CreateMatrix(int rowCount, int columsCount)
+{
+    int[,] matrix = new int[rowCount, columsCount];
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+// Функция вывода элементов двумерного массива целых чисел на консоль
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+// **********   Тело программы   **********
+int[,] matrix = CreateMatrix(4, 5);
+ShowMatrix(matrix);
