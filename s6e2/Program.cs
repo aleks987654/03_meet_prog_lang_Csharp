0fbcad009e592f024321405e41d5b924a09fe31d
Aleksandr Trushkov <aleksmount@yandex.ru>
2024-04-03 22:05:51 +0700

    add s6e2 - str in arr_char, unicode

---
+++ s6e2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Семинар 6. Массивы и строки
+// Задание 2
+// На основе символов строки (тип string) 
+// сформировать массив символов (тип char []). 
+// Вывести массив на экран
+
+// !!!  Указание: Метод строки ЕщСрфкФккфн() не использовать
+// Пример: "Hello!" => ['H', 'e', 'l', 'l', 'o', '!]'
+
+using System;
+using System.Text;
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+
+char[] ArrayCharsFromString(string str)
+{
+    // int l = str.Length;
+    char[] chars = new char[str.Length];
+    for (int i = 0; i < str.Length; i++)
+    {
+        chars[i] = str[i];
+    }
+    return chars;
+}
+
+void PrintArrayChars(char[] chars)
+{
+    foreach (var item in chars)
+    {
+        Console.Write($"{item}\t");
+    }
+}
+
+string n = Console.ReadLine()!;
+PrintArrayChars(ArrayCharsFromString(n));
