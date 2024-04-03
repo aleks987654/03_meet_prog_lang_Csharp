4086af22722dea1c77ff60e9d84378214fc19b00
Aleksandr Trushkov <aleksmount@yandex.ru>
2024-04-03 20:15:10 +0700

    add s6e1 - string from array of chars

---
+++ s6e1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Семинар 6. Массивы и строки
+// Задание 1
+// Задайте массив символов (тип char []). 
+// Создать строку из символов этого массива.
+// !!!  Указание: Конструктор строки вида string(char[]) не использовать
+// Пример: ['a', 'b', 'c', 'd'] => "abcd"
+
+// Функция создания строки из элементов массива символов
+string CharsOfString(char[] arr)
+{
+    string st = "";
+    foreach (var i in arr)
+    {
+        st += i; // st=st+i
+    }
+    return st;
+}
+
+char[] chars = new char[] { 'a', 'b', 'c', 'd' };
+Console.WriteLine(CharsOfString(chars));
