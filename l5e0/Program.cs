4ac9d863675e394ece9694fc77cd27b7c850a09a
Aleksandr Trushkov <aleksmount@yandex.ru>
2024-04-06 15:50:11 +0700

    add l5e0 - recursion matryoshka

---
+++ l5e0/Program.cs
@@ -0,0 +1,31 @@
+﻿// Лекция 5. Рекурсия
+// Пример 0
+
+void OpenMatryoshka(int size)
+{
+    // Базовый случай: самая маленькая матрёшка
+    if (size == 1)
+    {
+        // Console.WriteLine("Reaches the smallest matryoshka!");
+        return;
+    }
+    Console.WriteLine($"Opening matryoshka of size {size}...");
+    OpenMatryoshka(size - 1);
+}
+
+void rOpenMatryoshka(int size)
+{
+    // Базовый случай: самая маленькая матрёшка
+    if (size == 1)
+    {
+        // Console.WriteLine("Reaches the smallest matryoshka!");
+        return;
+    }
+    rOpenMatryoshka(size - 1);    
+    Console.WriteLine($"Opening matryoshka of size {size}...");
+}
+
+// **********     Тело программы     **********
+OpenMatryoshka(5);
+Console.WriteLine();
+rOpenMatryoshka(5);
