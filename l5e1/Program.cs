64fb3824c7d65901f0b5a33a59920f579e048038
Aleksandr Trushkov <aleksmount@yandex.ru>
2024-04-06 16:17:41 +0700

    l5e1 add debug console prints

--- l5e1/Program.cs
+++ l5e1/Program.cs
@@ -7,9 +7,13 @@ int Fact(int n)
 {
     if (n == 1 || n == 0)
     {
+        Console.WriteLine($"Stop recursion: n = {n}");
         return 1;
     }
-    return n * Fact(n - 1);
+    Console.WriteLine(n);
+    int f = Fact(n - 1);
+    Console.WriteLine($"Возврат: n = {n}, fact = {f}");    
+    return n * f;
 }
 
-Console.Write(Fact(0));
+Console.Write(Fact(5));
